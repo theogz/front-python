9313c3675c2a564a3f47bcca0f06203164443d48
Theo Gantzer <theo.gantzer@gmail.com>
2020-01-23 14:27:09 -0800

    :wrench: contacts should be email-centric

--- util/contacts.py
+++ util/contacts.py
@@ -1,7 +1,7 @@
 from .helpers import get
 
 
-def fetch_contact(contact_id):
-    route = f'/contacts/{contact_id}'
+def fetch_contact(handle):
+    route = f'/contacts/alt:email:{handle}'
 
     return get(route, attempt=1, max_attempts=1)
